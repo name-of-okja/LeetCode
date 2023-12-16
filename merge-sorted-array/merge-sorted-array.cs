5460635866074c7e235b112f4f0aab72aa6d1e1d
OkJa <151524504+name-of-okja@users.noreply.github.com>
2023-12-16 13:07:14 +0900

    Time: 107 ms, Memory: 42.9 MB - LeetHub

--- merge-sorted-array/merge-sorted-array.cs
+++ merge-sorted-array/merge-sorted-array.cs
@@ -4,21 +4,16 @@ public class Solution {
         int nums1Idx = m - 1;
         int nums2Idx = n - 1;
 
-        while (nums1Idx >= 0 && nums2Idx >= 0)
+        while (nums2Idx >= 0)
         {
-            if (nums1[nums1Idx] < nums2[nums2Idx])
-            {
-                nums1[idx--] = nums2[nums2Idx--];
-            }
-            else
+            if (nums1Idx >= 0 && nums1[nums1Idx] > nums2[nums2Idx])
             {
                 nums1[idx--] = nums1[nums1Idx--];
             }
-        }
-
-        while (nums2Idx >= 0)
-        {
-            nums1[idx--] = nums2[nums2Idx--];
+            else
+            {
+                nums1[idx--] = nums2[nums2Idx--];
+            }
         }
     }
 }
