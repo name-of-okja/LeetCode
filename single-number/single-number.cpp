d052bb6783f302ed64cf76b84fcba5532933f61b
OkJa <151524504+name-of-okja@users.noreply.github.com>
2024-01-10 13:39:53 +0900

    Time: 16 ms, Memory: 17.2 MB - LeetHub

--- single-number/single-number.cpp
+++ single-number/single-number.cpp
@@ -1,33 +1,13 @@
 class Solution {
 public:
-    int continueIndex(vector<int> &nums, int pos)
-    {
-        for (int i = pos + 1; i < nums.size(); ++i)
-        {
-            if (nums[pos] != nums[i])
-            {
-                return i;
-            }
-        }
-
-        return pos + 1;
-    }
-
     int singleNumber(vector<int> &nums)
     {
-        sort(nums.begin(), nums.end());
-
-        int pos = 0;
-        while (true)
+        // Wow....
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++)
         {
-            int nextIndex = continueIndex(nums, pos);
-
-            if (pos + 1 == nextIndex)
-            {
-                return nums[pos];
-            }
-
-            pos = nextIndex;
+            ans = ans ^ nums[i];
         }
+        return ans;
     }
 };
