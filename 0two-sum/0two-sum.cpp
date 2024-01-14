7c6e68be791622286f472d9f745ff983383cc5c8
OkJa <151524504+name-of-okja@users.noreply.github.com>
2024-01-14 12:01:20 +0900

    Time: 167 ms, Memory: 10.5 MB - LeetHub

---
+++ 0two-sum/0two-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int m = target - nums[i];
+
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                if (nums[j] == m)
+                {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
