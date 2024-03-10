ad210597973de43adf67489d17f37a4eb5f0dc4f
OkJa <151524504+name-of-okja@users.noreply.github.com>
2024-03-10 16:27:06 +0900

    Time: 0 ms, Memory: 7.5 MB - LeetHub

--- pascals-triangle-ii/pascals-triangle-ii.cpp
+++ pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -9,7 +9,7 @@ public:
         // row0 :: row0[0] = 1;
         // row1 :: row0[0], (0 + row0[0]) = 1, 1;
         // row2 :: row0[0], (0 + row1[0] + row1[1]), (0 + row1[1]) = 1, 2, 1;
-        for (int i = 1; i < rowIndex + 1; i++)
+        for (int i = 1; i <= rowIndex; i++)
         {
             for (int j = i; j >= 1; j--)
             {
