d8b662237d2cc06da4bb115a5e130cc9f48ebf3d
OkJa <151524504+name-of-okja@users.noreply.github.com>
2024-09-17 20:48:22 +0900

    Time: 47 ms (33.89%), Space: 94.3 MB (30.7%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -3,38 +3,21 @@ class Solution
 public:
     bool repeatedSubstringPattern(string s)
     {
-        if (s.size() == 1)
+        int n = s.length();
+        for (int i = 1; i <= n / 2; ++i)
         {
-            return false;
-        }
-
-        string reapet;
-        reapet.push_back(s[0]);
-        int n = s.size() / 2;
-        for (int i = 1; i < n; ++i)
-        {
-            if (s[i] != s[0])
+            if (n % i == 0)
             {
-                reapet.push_back(s[i]);
-            }
-            else
-            {
-                break;
+                string substring = s.substr(0, i);
+                string repeated = "";
+                for (int j = 0; j < n / i; ++j)
+                {
+                    repeated += substring;
+                }
+                if (repeated == s)
+                    return true;
             }
         }
-
-        if (s.size() % reapet.size() != 0)
-        {
-            return false;
-        }
-
-        int repeatedCount = s.size() / reapet.size();
-        string result;
-        for (int i = 0; i < repeatedCount; ++i)
-        {
-            result += reapet;
-        }
-
-        return result == s;
+        return false;
     }
 };
